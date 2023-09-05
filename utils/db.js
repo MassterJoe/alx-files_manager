3b7f136f735d079cd2e8ba4458bcb86d6c2ed7e2
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-09-05 13:30:23 -0400

    niec

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import { MongoClient } from "mongodb";
+const host = process.env.DB_HOST || "localhost";
+const port = process.env.DB_PORT || "27017";
+const dbName = process.env.DB_DATABASE || "files_manager";
+const url = `mongodb://${host}:${port}`;
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client
+      .connect()
+      .then(() => {
+        this.db = this.client.db(`${dbName}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  isAlive() {
+    if (this.client.isConnected()) {
+      return true;
+    }
+    return false;
+  }
+
+  async nbUsers() {
+    const users = this.db.collection("users");
+    const numUsers = await users.countDocuments();
+    return numUsers;
+  }
+  async nbFiles() {
+    const files = this.db.collection("files");
+    const numFIles = await files.countDocuments();
+    return numFIles;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
